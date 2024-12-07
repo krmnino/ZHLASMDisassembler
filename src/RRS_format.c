26e2f54f08d2585676c20bf85a60cf90f78d8a3b
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2024-12-07 14:50:45 -0500

    fix typo in comment

--- src/RRS_format.c
+++ src/RRS_format.c
@@ -141,7 +141,7 @@ int build_RRS(size_t table_index, const char* operands_token, uint8_t* bin_buffe
     // D4: bits(20-31)
     bin_buffer[2] = bin_buffer[2] | (d4 >> 8);
     bin_buffer[3] = d4 & 0x0FF;
-    // M3: bits(20-31)
+    // M3: bits(32-35)
     bin_buffer[4] = m3 << 4;
      // Opcode (part 2): bits(40-47)
     bin_buffer[5] = opcode & 0x00FF;
