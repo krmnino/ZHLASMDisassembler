fff21c0f13004ebf2a72802774f76cbc9efd51bc
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2024-12-28 12:59:34 -0500

    print instruction offset

--- src/VRV_format.c
+++ src/VRV_format.c
@@ -250,6 +250,7 @@ ErrorCode display_VRV(Context* c, Instruction* instr){
     printf("BINARY:   %s\n", conv_buffer);
     printf("LENGTH:   0x%x\n", length);
     printf("FORMAT:   VRV\n");
+    printf("OFFSET:   0x%lx\n", instr->offset);
     // Print operands
     hex_str_2_char_str(((void*)&instr->binary), MAX_INSTRUCTION_LEN, 1, conv_buffer, MAX_PRINTOUT_FIELD_LEN, 1, NO_SKIP, false);
     printf("V1:       %s\n", conv_buffer);
